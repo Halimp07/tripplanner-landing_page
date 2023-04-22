e18ee3e96246a4d77cec61f47f942961439aedc0
Halimp07 <halimputra0701@gmail.com>
2023-04-22 20:00:29 +0700

    first commit

---
+++ src/data/data.jsx
@@ -0,0 +1,18 @@
+// Import images for HeroHome
+import feature_1 from "../images/prototype_1.png";
+import feature_2 from "../images/prototype_2.png";
+import feature_3 from "../images/prototype_3.png";
+
+// import videos for HeroHome
+import video from "../videos/video.mp4";
+
+// Export Video using declare variable
+export const VideoPlay = {
+  videoPopUp: video,
+};
+
+export const ImageFeatures = {
+  image_1: feature_1,
+  image_2: feature_2,
+  image_3: feature_3,
+};
